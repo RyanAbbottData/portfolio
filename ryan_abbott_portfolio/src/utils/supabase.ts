c92571652e30251dd97393369e5ccbd58e66c109
RyanAbbottData <ryanabbott855@gmail.com>
2026-05-11 16:38:13 -0500

    removing legacy folder and repushing config files

--- ryan_abbott_portfolio/src/utils/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY as string
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export default supabase
